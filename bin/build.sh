cc6f80cce22d237924a52fcd13498433243576d5
Gabor Ecker <ecker.gabor@gmail.com>
2019-10-16 22:31:46 +0200

    PDF konverter bekötve az automatikus buildbe.

--- bin/build.sh
+++ bin/build.sh
@@ -12,6 +12,7 @@ mkdir dist/
 python3 bin/convert.py opensong --from-dir songs/ --to-dir dist/Emmánuel/
 python3 bin/convert.py json --from-dir songs/ --to dist/songs.json
 python3 bin/convert.py diatar --from-dir songs/ --to dist/emmanuel.dtx
+python3 bin/convert.py pdf --from-dir songs/ --to dist/emmet_offline.pdf
 
 # Done this way to avoid having to install 'zip'. It's equivalent to:
 # cd dist/Emmánuel/
@@ -20,9 +21,10 @@ python3 bin/convert.py diatar --from-dir songs/ --to dist/emmanuel.dtx
 python3 -c "import shutil; shutil.make_archive(base_name='dist/opensong-enekek', format='zip', root_dir='dist', base_dir='Emmánuel')"
 
 if [[ $BB_AUTH_STRING ]]; then
-    curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/opensong-enekek.zip"
-    curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/songs.json"
-    curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/emmanuel.dtx"
+    curl -v -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/opensong-enekek.zip"
+    curl -v -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/songs.json"
+    curl -v -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/emmanuel.dtx"
+    curl -v -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"dist/emmet_offline.pdf"
 else
     echo "No BB_AUTH_STRING variable found, not deploying artifacts."
 fi
